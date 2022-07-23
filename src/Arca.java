154201f98a5374cf8a264025277cfa4debe4e5a4
GreedlyCore <naganariselves@gmail.com>
2022-07-23 13:47:41 +0300

    FirstInit

---
+++ src/Arca.java
@@ -0,0 +1,21 @@
+public class Arca {
+    
+    public static void main(String[] args) {
+        
+    }
+
+
+
+}
+
+class Dog{
+
+    private int age;
+    private String name;
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+
+}
