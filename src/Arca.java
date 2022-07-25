2cc2ce8e89c741891523f0a89816d9640992e27a
GreedlyCore <naganariselves@gmail.com>
2022-07-26 01:43:09 +0300

    Init2

--- src/Arca.java
+++ src/Arca.java
@@ -1,7 +1,11 @@
 public class Arca {
     
     public static void main(String[] args) {
-        
+     
+        Dog dog = new Dog("Dylan", 130);
+        System.out.println(dog.getAge());
+        System.out.println("okay");
+
     }
 
 
@@ -13,8 +17,22 @@ class Dog{
     private int age;
     private String name;
 
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
     public void setAge(int age){
         this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
     }
 
 
